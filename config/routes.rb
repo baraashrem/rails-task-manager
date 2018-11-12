d389c65839fa944bff3658d2b2b211b6b5b5ffd9
Baraa Shrem <baraa.shrem@gmail.com>
2018-11-12 14:51:50 +0200

    added tasks views for show and index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+ # CRUD actions: VERB url-name to: 'Controller#action'
+ root to: 'tasks#index'
+ # list all tasks - index
+ get 'tasks', to: 'tasks#index'
+ # view one task details - show with :id dynamic part find(params:[:id])
+ get 'tasks/:id', to: 'tasks#show'
+ # add a new task - create  - gonna take new and save
+ get 'new', to: 'tasks#new'
+ # update a task - takes edit and save
+ # remove a task - destroy / delete
 end
